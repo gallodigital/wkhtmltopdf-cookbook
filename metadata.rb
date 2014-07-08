e2ebba8ec9a14a234f840611bfe0aeed6235a630
Keith Gable <keith@gallodigital.com>
2014-07-08 17:16:11 -0500

    Update metadata.

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,6 @@
-maintainer       "Arkency"
-maintainer_email "dev@arkency.com"
+name             "wkhtmltopdf"
+maintainer       "Keith Gable"
+maintainer_email "keith@gallodigital.com"
 license          "Apache 2.0"
 description      "Installs wkhtmltopdf"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
