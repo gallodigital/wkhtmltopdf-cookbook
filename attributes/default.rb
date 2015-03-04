f24803048e787acbb88053d22b99d52bbcc42365
Keith Gable <keith@gallodigital.com>
2015-03-04 11:00:53 -0800

    Bump wkhtmltopdf version.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 # FIXME: Make this support other platforms better.
 default['wkhtmltopdf']['arch'] = "amd64"
 default['wkhtmltopdf']['platform'] = "centos6"
-default['wkhtmltopdf']['version'] = "0.12.1"
+default['wkhtmltopdf']['version'] = "0.12.2.1"
 default['wkhtmltopdf']['static_download_url'] = "http://downloads.sourceforge.net/project/wkhtmltopdf/#{node['wkhtmltopdf']['version']}/wkhtmltox-#{node['wkhtmltopdf']['version']}_linux-#{node['wkhtmltopdf']['platform']}-#{node['wkhtmltopdf']['arch']}.rpm"
