7143da0bd6359ec00f43f6e1b415503c8234f1f4
Keith Gable <keith@gallodigital.com>
2015-03-05 10:43:50 -0800

    Define the package file separately so we can use it in a cookbook_file.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,5 @@
 default['wkhtmltopdf']['arch'] = "amd64"
 default['wkhtmltopdf']['platform'] = "centos6"
 default['wkhtmltopdf']['version'] = "0.12.2.1"
-default['wkhtmltopdf']['static_download_url'] = "http://downloads.sourceforge.net/project/wkhtmltopdf/#{node['wkhtmltopdf']['version']}/wkhtmltox-#{node['wkhtmltopdf']['version']}_linux-#{node['wkhtmltopdf']['platform']}-#{node['wkhtmltopdf']['arch']}.rpm"
+default['wkhtmltopdf']['package_file'] = "wkhtmltox-#{node['wkhtmltopdf']['version']}_linux-#{node['wkhtmltopdf']['platform']}-#{node['wkhtmltopdf']['arch']}.rpm"
+default['wkhtmltopdf']['static_download_url'] = "http://downloads.sourceforge.net/project/wkhtmltopdf/#{node['wkhtmltopdf']['version']}/#{node['wkhtmltopdf']['package_file']}"
